a8b2b3bbf261280ca95ce6931ec0ca00781f39e8
timmybobguy <41880797+timmybobguy@users.noreply.github.com>
2019-08-07 15:36:57 +1200

    Adding basic window

---
+++ SongBookApp/SongBookApp/StartScreen.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SongBookApp
+{
+    public partial class StartScreen : Form
+    {
+        public StartScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("quit command recieved");
+            Application.Exit();
+        }
+    }
+}
